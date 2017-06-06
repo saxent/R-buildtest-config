9bd3ce95f6b931d2222f440791c902e0ed88eb8c
Vijay Singh <Vijay.Singh@Pfizer.com>
2017-06-06 10:51:28 -0400

    Adding R Test Scripts for abc

---
+++ R/code/abc/cv4postpr.R
@@ -0,0 +1,9 @@
+require(abc.data)
+data(human)
+###Reduce the sample size of the simulations to reduce the running time.
+###Do not do that with your own data!
+ss<-c(1:1000,50001:51000,100001:101000)
+cv.modsel <- cv4postpr(models[ss], stat.3pops.sim[ss,], nval=5, tols=c(.05,.1), method="rejection")
+summary(cv.modsel)
+plot(cv.modsel, names.arg=c("Bottleneck", "Constant", "Exponential"))
+
